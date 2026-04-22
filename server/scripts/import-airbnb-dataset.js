20503b140f615e5655b15fc1bab1735c32a1037c
pfelix7 <perrefelix@gmail.com>
2026-04-22 11:38:23 -0400

    added the dataset

--- server/scripts/import-airbnb-dataset.js
+++ server/scripts/import-airbnb-dataset.js
@@ -13,7 +13,7 @@ const Review = require("../models/Review");
 
 function parseArgs(argv) {
   const args = {
-    file: path.resolve(__dirname, "../../airbnb.listingAndReviews.json"),
+    file: path.resolve(__dirname, "../data/airbnb-listings.json"),
     reset: false,
   };
 
